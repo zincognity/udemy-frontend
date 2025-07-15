84d38a0af6a4beb5f45e6a052e780174068391e0
zincognity <67128655+zincognity@users.noreply.github.com>
2025-07-15 18:56:04 -0500

    feat: Adapter has been added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    site: "https://udemy.incognity.dev",
+    output: "server",
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  integrations: [react()]
+    integrations: [react()],
+
+    adapter: node({
+        mode: "standalone",
+    }),
+
+    server: {
+        host: true,
+    },
 });
